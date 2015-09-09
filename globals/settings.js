7665717db33d796c2a3f5c5b6a24a81520f2beeb
JeffWu <jeff@miniasp.com>
2015-09-09 17:11:05 +0800

    Edit config/windows.json Init globals/settings.js

---
+++ globals/settings.js
@@ -0,0 +1,16 @@
+/**
+ * @nightwatch settings
+ * @authors Your Name (you@example.org)
+ * @date    2015-09-09 16:08:48
+ * @version $Id$
+ */
+
+var testInfo = {
+	google : 'http://www.google.com.tw',
+	yahoo : 'http://www.yahoo.com.tw'
+};
+
+module.exports = {
+	google : testInfo.google,
+	yahoo : testInfo.yahoo
+};
